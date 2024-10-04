b498405adf3bfc758f36831bd5c184b116aa7286
Matheus Carvalho <124722630+matheussdcarvalho@users.noreply.github.com>
2024-10-03 21:32:58 -0300

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbConfig = "mongodb+srv://matheusflemis:zeroberto@cluster0.pq3kf.mongodb.net/annotations?retryWrites=true&w=majority&appName=Cluster0"
+const dbConfig = "***"
 
 const connection = mongoose.connect(dbConfig);
 
